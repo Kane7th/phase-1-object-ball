f29755e3b40f15a308296f5395b429afba364ea5
Kane Kabena <onekaneldn@gmail.com>
2025-03-07 11:39:51 +0300

    Intro to object ball - first iteration

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,8 +1,8 @@
 function gameObject(){
     let obj = {
         home:{
-            teamName: "Brooklyn Nets" 
-            colors: ["black","white"]
+            teamName: "Brooklyn Nets" ,
+            colors: ["black","white"],
             players: {
                 "Alan Anderson" : {
                     number : 0,
@@ -13,50 +13,105 @@ function gameObject(){
                     steals : 3,
                     blocks : 1,
                     slamDunks : 1,
-
                 },
                 "Reggie Evans" : {
-                    number : ,
-                    shoe : ,
-                    points : ,
-                    rebounds : ,
-                    assists : ,
-                    steals : ,
-                    blocks : ,
-                    slamDunks : ,
-
-                }
-            }
+                    number : 30,
+                    shoe : 14,
+                    points : 12,
+                    rebounds : 12,
+                    assists : 12,
+                    steals : 12,
+                    blocks : 12,
+                    slamDunks : 7,
+                },
+                "Brook Lopez" : {
+                    number : 11,
+                    shoe : 17,
+                    points : 17,
+                    rebounds : 19,
+                    assists : 10,
+                    steals : 3,
+                    blocks : 1,
+                    slamDunks : 15,
+                },
+                "Mason Plumlee" : {
+                    number : 1,
+                    shoe : 19,
+                    points : 26,
+                    rebounds : 12,
+                    assists : 6,
+                    steals : 3,
+                    blocks : 8,
+                    slamDunks : 5,
+                },
+                "Jason Terry" : {
+                    number : 31,
+                    shoe : 15,
+                    points : 19,
+                    rebounds : 2,
+                    assists : 2,
+                    steals : 4,
+                    blocks : 11,
+                    slamDunks : 1,
+                },
+            },
         },
 
-
         away:{
-            teamName: "Brooklyn Nets" 
-            colors: ["black","white"]
+            teamName: "Charlotte Hornets" ,
+            colors: ["turquoise","purple"],
             players: {
-                "Alan Anderson" : {
+                "Jeff Adrien" : {
+                    number : 4,
+                    shoe : 18,
+                    points : 10,
+                    rebounds : 1,
+                    assists : 1,
+                    steals : 2,
+                    blocks : 7,
+                    slamDunks : 2,
+                },
+                "Bismak Biyombo" : {
                     number : 0,
                     shoe : 16,
-                    points : 22,
+                    points : 12,
+                    rebounds : 4,
+                    assists : 7,
+                    steals : 7,
+                    blocks : 15,
+                    slamDunks : 10,
+                },
+                "DeSagna Diop" : {
+                    number : 2,
+                    shoe : 14,
+                    points : 24,
                     rebounds : 12,
                     assists : 12,
-                    steals : 3,
-                    blocks : 1,
-                    slamDunks : 1,
-
+                    steals : 4,
+                    blocks : 5,
+                    slamDunks : 5,
                 },
-                "Reggie Evans" : {
-                    number : ,
-                    shoe : ,
-                    points : ,
-                    rebounds : ,
-                    assists : ,
-                    steals : ,
-                    blocks : ,
-                    slamDunks : ,
-
-                }
-            }
+                "Ben Gordon" : {
+                    number : 8,
+                    shoe : 15,
+                    points : 33,
+                    rebounds : 3,
+                    assists : 2,
+                    steals : 1,
+                    blocks : 1,
+                    slamDunks : 0,
+                },
+                "Brendan Haywood" : {
+                    number : 33,
+                    shoe : 15,
+                    points : 6,
+                    rebounds : 12,
+                    assists : 12,
+                    steals : 22,
+                    blocks : 5,
+                    slamDunks : 12,
+                },
+            },
         }
     }
     return obj
@@ -64,6 +119,7 @@ function gameObject(){
 
 console.log(gameObject());
 
+// -----------------------------------------------------------------------------------
 
 function getPlayerNumber(name){
     let object = gameObject();
@@ -75,3 +131,8 @@ function getPlayerNumber(name){
         return "Not this player!"
     }
 }
+
+console.log(getPlayerNumber("Ben Gordon"))
+
+//------------------------------------------------------------------------------------
+
